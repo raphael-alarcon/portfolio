1f5d1ee011c1e999be3aaa5cc45da60352f9b2bb
ralarcon <raphael.alarcon@etu.u-bordeaux.fr>
2022-10-19 22:56:00 +0200

    ajout loader

--- js/utils.js
+++ js/utils.js
@@ -9,14 +9,6 @@ function scrollBar() {
     if (scrolled >= 99)document.getElementById("progressbar__content").style.width = "101%";
 } 
 
-// ScrollReveal
-ScrollReveal().reveal('.presentation', {
-    delay: 500,
-    useDelay: 'onload',
-    reset: true,
-});
-
-
 // Copy Email
 function copyEmail() {
   
@@ -28,9 +20,7 @@ function copyEmail() {
 }
 
 
-
 // Change language
-
 function switchLang(lang)
 {
     $("[data-" + lang + "]").text(function(i, e) {
@@ -41,20 +31,46 @@ function switchLang(lang)
 switchLang("en");
 
 $(".switchlang").click(function() {
-    // change the button caption here, eg a flag
-    // UX opinion of whether it should be what it is 
-    // or what it will become
-    // ie "de" click to make it "de"
-    // or "de" it's currently "de", click to change it
-    
-    // switch to other language based on language on the button
+    loading();
     var lang = $(this).data("lang") == "fr" ? "en" : "fr";
     $(this).data("lang", lang);
     $(".switchlang img").attr("src", flags[lang]);
-    switchLang(lang)
+    switchLang(lang);
 });
 
 var flags = {
     "en": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/800px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png",
     "fr": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Flag_of_France_%28lighter_variant%29.svg/250px-Flag_of_France_%28lighter_variant%29.svg.png"
+}
+
+
+
+// Loader
+let i = 0;
+var myInterval;
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    loading();
+});
+
+function loading() {
+    console.log("Loading...");
+    i = 0;
+    $(".loader").css("display", "flex");
+    $(".loader").css("opacity", "1");
+    $(".loader-base div").css("width",0);
+    myInterval = setInterval( () => {
+      moveLoading();
+    }, 20);
+}
+
+function moveLoading() {
+  $(".loader-base div").css("width", i++ + "%");
+  if (i == 101) {
+    clearInterval(myInterval);
+    $(".loader").css("opacity", "0");
+    setTimeout(() => {
+        $(".loader").css("display", "none");
+    }, 500);
+  }
 }
